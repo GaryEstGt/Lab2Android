16ede390fd4d313aad8a9f344820b32749cd29a6
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2018-09-12 20:46:07 -0600

    comit 1

--- app/src/main/java/com/example/garya/lab2android/MainActivity.java
+++ app/src/main/java/com/example/garya/lab2android/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.garya.lab2android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textView)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnElegir)
+    public void onViewClicked() {
+        int VALOR_RETORNO = 1;
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        startActivityForResult(Intent.createChooser(intent, "Choose File"), VALOR_RETORNO);
     }
 }
