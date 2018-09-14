489034d260cfb63df05729a78e93419ffdd17f3a
Luis Farfán <luise.prado99@hotmail.com>
2018-09-14 12:19:17 -0600

    Arreglar lector

--- app/src/main/java/com/example/garya/lab2android/Lector.java
+++ app/src/main/java/com/example/garya/lab2android/Lector.java
@@ -1,16 +1,18 @@
 package com.example.garya.lab2android;
 
+import android.app.Application;
 import android.net.Uri;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class Lector {
 
-    public static String LeerArchivo(Uri archivo) throws IOException{
-        InputStream IS = getContentResolver().openInputStream(archivo);
+    public static String LeerArchivo(Application application, Uri archivo) throws IOException{
+        InputStream IS = application.getContentResolver().openInputStream(archivo);
         BufferedReader BR = new BufferedReader(new InputStreamReader(IS));
         StringBuilder SB = new StringBuilder();
         String line = "";
