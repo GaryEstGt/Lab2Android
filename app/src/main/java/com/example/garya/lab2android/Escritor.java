56f873ae4a785be1fff2c8d28abd855a932d3b39
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2018-09-20 08:14:43 -0600

    laboratorio finalizado

--- app/src/main/java/com/example/garya/lab2android/Escritor.java
+++ app/src/main/java/com/example/garya/lab2android/Escritor.java
@@ -2,6 +2,7 @@ package com.example.garya.lab2android;
 
 import android.app.Application;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -24,11 +25,13 @@ public class Escritor {
         }
 
         try {
+            File chivo=new File(nuevo);
             FileWriter escritor=new FileWriter(nuevo);
             PrintWriter print=new PrintWriter(escritor);
             print.print(cadena);
             print.close();
             escritor.close();
+            chivo.createNewFile();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
